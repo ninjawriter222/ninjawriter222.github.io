341c722933e4f83617b3926c337a6ff6d43c9c13
ninjawriter222 <poe23002@byui.edu>
2020-04-01 16:08:03 -0700

    commit12

---
+++ assignments/lesson11/js/eventssodasprings.js
@@ -0,0 +1,25 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+.then((response) => response.json())
+.then((jsObject) => 
+{
+    console.log(jsObject);
+    const towns = jsObject["towns"];
+    for (let i = 0; i < towns.length; i++) 
+    {
+                if (towns[i].name == "Soda Springs") 
+                {
+                    let h2 = document.createElement('h2');
+                    h2.textContent = towns[i].name + " Upcoming events";
+                    document.querySelector('div.events').appendChild(h2);
+                    let events = towns[i].events;
+                    for (let x = 0; x < events.length; x++) 
+                    {
+                        let p = document.createElement('p');
+                        p.textContent = events[x];
+                        document.querySelector('div.events').appendChild(p);
+                    }
+                }
+    }
+});
