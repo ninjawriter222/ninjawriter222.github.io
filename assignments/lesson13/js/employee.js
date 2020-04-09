82a33530d13412b3324130851641ba38ca7a3578
ninjawriter222 <poe23002@byui.edu>
2020-04-08 19:46:50 -0700

    commit13.1

--- assignments/lesson13/js/employee.js
+++ assignments/lesson13/js/employee.js
@@ -1,5 +1,5 @@
 const apiURL =
-    'https://api.openweathermap.org/data/2.5/weather?lat=33.9&lon=-88.13&units=imperial&APPID=935cd4f7c41172a9c16edf60efa6bcfc';
+'https://ninjawriter222.github.io/assignments/lesson13/json/json.json';
 
 fetch(apiURL)
     .then((response) => response.json())
@@ -7,26 +7,36 @@ fetch(apiURL)
         console.log(jsObject);
         const currentTemp = document.querySelector('#current-temp');
 
-        const high = document.getElementById('high');
-        high.textContent = jsObject.main.temp_max;
-        var high1 = jsObject.main.temp_max;
-        const low = document.getElementById('low');
-        low.textContent = jsObject.main.temp_min;
-        var low1 = jsObject.main.temp_min;
-        const humidity = document.getElementById('humidity');
-        humidity.textContent = jsObject.main.humidity;
-        const wSpeed = document.getElementById('wSpeed');
-        wSpeed.textContent = jsObject.wind.speed;
-        var wSpeed1 = jsObject.wind.speed;
-        const element = document.getElementById('element');
-        element.textContent = jsObject.weather["0"].main;
+        const name1 = document.getElementById('name1');
+        name1.textContent = jsObject.employees[0].name;
+        const age1 = document.getElementById('age1');
+        age1.textContent = jsObject.employees[0].age;
+        const phoneNumber1 = document.getElementById('phoneNumber1');
+        phoneNumber1.textContent = jsObject.employees[0].phoneNumber;
+        const email1 = document.getElementById('email1');
+        email1.textContent = jsObject.employees[0].email;
+        const certLv1 = document.getElementById('certLv1');
+        certLv1.textContent = jsObject.employees[0].certLV;
 
-        var tempaverage = ((high1 + low1) / 2);
+        const name2 = document.getElementById('name2');
+        name2.textContent = jsObject.employees[1].name;
+        const age2 = document.getElementById('age2');
+        age2.textContent = jsObject.employees[1].age;
+        const phoneNumber2 = document.getElementById('phoneNumber2');
+        phoneNumber2.textContent = jsObject.employees[1].phoneNumber;
+        const email2 = document.getElementById('email2');
+        email2.textContent = jsObject.employees[1].email;
+        const certLv2 = document.getElementById('certLv2');
+        certLv2.textContent = jsObject.employees[1].certLV;
 
-        
-            windChill = 35.74 + 0.6215 * tempaverage - 35.75 * Math.pow(wSpeed1, 0.16) + 0.4275 * tempaverage * Math.pow(wSpeed1, 0.16);
-            windChill = Math.round(windChill) + "&deg;F";
-        
-        document.getElementById("windChill").innerHTML = windChill;
-
+        const name3 = document.getElementById('name3');
+        name3.textContent = jsObject.employees[2].name;
+        const age3 = document.getElementById('age3');
+        age3.textContent = jsObject.employees[2].age;
+        const phoneNumber3 = document.getElementById('phoneNumber3');
+        phoneNumber3.textContent = jsObject.employees[2].phoneNumber;
+        const email3 = document.getElementById('email3');
+        email3.textContent = jsObject.employees[2].email;
+        const certLv3 = document.getElementById('certLv3');
+        certLv3.textContent = jsObject.employees[2].certLV;
     });
